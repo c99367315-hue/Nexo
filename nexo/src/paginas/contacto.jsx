5201cdfea532697bb860f8e5ee256292cd211504
c99367315-hue <c99367315@gmail.com>
2026-06-10 16:16:07 -0500

    Secciones

---
+++ nexo/src/paginas/contacto.jsx
@@ -0,0 +1,12 @@
+import Menu from "../componentes/menu"
+
+function Contacto() {
+    return (
+        <div>
+            <Menu />
+            <h1>Bienvenido a la pagina de contacto</h1>
+        </div>
+    )
+}
+
+export default Contacto
---
+++ nexo/src/paginas/registro.jsx
@@ -0,0 +1,12 @@
+import Menu from "../componentes/menu"
+
+function Registro() {
+    return (
+        <div>
+            <Menu />
+            <h1>Bienvenido a la pagina de registro</h1>
+        </div>
+    )
+}
+
+export default Registro
